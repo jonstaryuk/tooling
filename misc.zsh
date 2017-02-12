6a889e5b208934c3d4e343fe5b9f345f7b74bcf0
Jon Staryuk <jstaryuk@gmail.com>
2017-02-12 15:23:58 -0800

    Add base16-shell setup

--- misc.zsh
+++ misc.zsh
@@ -5,5 +5,8 @@ alias rand='random'
 source '/Users/jon/Applications/google-cloud-sdk/path.zsh.inc'
 source '/Users/jon/Applications/google-cloud-sdk/completion.zsh.inc'
 
+BASE16_SHELL=$HOME/.config/base16-shell/
+[ -n "$PS1" ] && [ -s $BASE16_SHELL/profile_helper.sh ] && eval "$($BASE16_SHELL/profile_helper.sh)"
+
 export PATH=$PATH:/Users/jon/code/go/bin
 
