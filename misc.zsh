77f7068642db6d6e4f07d7ba4dddbfeda121a343
Jon Staryuk <jstaryuk@gmail.com>
2017-07-22 12:34:43 -0700

    Add optimizePdf Ghostscript alias

--- misc.zsh
+++ misc.zsh
@@ -28,3 +28,13 @@ function share() {
         return 1
     fi
 }
+
+function optimizePdf() {
+    filepath=$1
+
+    \gs -dNOPAUSE -dBATCH -sDEVICE=pdfwrite -dCompatibilityLevel=1.4 \
+        -dPDFSETTINGS=/ebook -dNumRenderingThreads=4 \
+        -dBandBufferSpace=500000000 -dBufferSpace=1000000000 \
+        -sOutputFile="${1%.pdf}.opt.pdf" \
+        -c "1000000000 setvmthreshold" -f "$1"
+}
